26fb861ac637f05e247a8b59b547799e6a8256d1
JacobYolles <jacob.yolles@ucdenver.edu>
2018-10-01 17:09:59 -0600

    added base layer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ShowCard from "./components/ShowCard";
+import Title from "./components/Title"
+import Container from "./Container";
+import Row from "./Row";
+import Column from "./Column";
+import Wrapper from "./components/Wrapper";
+import shows from "./shows.json";
 import './App.css';
 
 class App extends Component {
+
+  // create my apps state
+  state = {
+    shows,
+    currentScore: 0,
+    topScore: 0,
+    correctIncorrect: "",
+    clicked: [],
+  };
+
+// Create all my handles. 
+
+
+
+
+// Create my renderizing.
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+<Wrapper>
+      <Title>
+        Click on an image to earn points, but don't click on one more than once!
+      </Title>
+      <Container>
+        <Row>
+          {this.state.shows.map(show => (
+            <Column size="md-3 sm-6">
+              <ShowCard
+                key = {show.id}
+                handleClick={this.handleClick}
+                handleIncrement={this.handleIncrement}
+                handleReset={this.handleReset}
+                handleShuffle={this.handleShuffle}
+                title={show.title}
+                id={show.id}
+                image={show.image}
+            />
+            </Column>
+          ))}
+        </Row>
+      </Container>
+      </Wrapper>
     );
   }
 }
---
+++ src/components/ShowCard/ShowCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./ShowCard.css";
+
+const ShowCard = props => (
+  <div 
+  className="card"
+  value = {props.id}
+ 
+  onClick={() => props.handleClick(props.id)}
+  >
+   <div className="content">
+  <strong>Name:</strong> {props.title}
+  </div>
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
+    </div>
+  </div>
+);
+
+export default ShowCard;
