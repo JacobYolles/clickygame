004cec08abea82c70cf3bc6dbf1ce904a4eac31b
JacobYolles <jacob.yolles@ucdenver.edu>
2018-10-01 19:12:52 -0600

    running

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import ShowCard from "./components/ShowCard";
 import Title from "./components/Title"
+import Nav from "./components/Nav";
 import Container from "./Container";
 import Row from "./Row";
 import Column from "./Column";
 import Wrapper from "./components/Wrapper";
 import shows from "./shows.json";
 import './App.css';
+
+// Create my random shuffling of cards.
+function randomShows(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
 
 class App extends Component {
 
@@ -21,13 +32,62 @@ class App extends Component {
 
 // Create all my handles. 
 
+// Handle the click function 
+handleClick = id => {
+  if (this.state.clicked.indexOf(id) === -1) {
+    this.handleIncrement();
+    this.setState({ clicked: this.state.clicked.concat(id) });
+  } else {
+    this.handleReset();
+  }
+};
 
+// Handle the incrementation of score and of clicking upon icons.
+handleIncrement = () => {
+  const newScore = this.state.currentScore + 1;
+  this.setState({
+    currentScore: newScore,
+    correctIncorrect: "You guessed correctly!"
+  });
+  if (newScore >= this.state.topScore) {
+    this.setState({ topScore: newScore });
+  }
+  else if (newScore === 12) {
+    this.setState({ correctIncorrect: "You win!" });
+  }
+  this.handleShuffle();
+};
+
+//Handle the reset of my random show cards positions.
+handleReset = () => {
+  this.setState({
+    currentScore: 0,
+    topScore: this.state.topScore,
+    correctIncorrect: "You guessed incorrectly!",
+    clicked: []
+  });
+  this.handleShuffle();
+};
+
+
+
+// Handle the shuffling of my random show cards.
+handleShuffle = () => {
+  let shuffledShows = randomShows(shows);
+  this.setState({ shows: shuffledShows });
+};
 
 
 // Create my renderizing.
   render() {
     return (
 <Wrapper>
+  <Nav
+  title="React Show Clicky Game" 
+  score={this.state.currentScore}
+  topScore={this.state.topScore}
+  correctIncorrect={this.state.correctIncorrect}
+  />
       <Title>
         Click on an image to earn points, but don't click on one more than once!
       </Title>
